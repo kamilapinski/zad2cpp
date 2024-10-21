c95b6021540a494fff62f0ae53b509d2f9f2a36b
Kamil Łapiński <kl459212@students.mimuw.edu.pl>
2024-10-21 13:32:46 +0200

    Podstawowa struktura

---
+++ strqueue.cpp
@@ -0,0 +1,33 @@
+#include "strqueue.h"
+
+unsigned long strqueue_new() {
+    // TODO
+}
+
+void strqueue_delete(unsigned long id) {
+    // TODO
+}
+
+size_t strqueue_size(unsigned long id) {
+    // TODO
+}
+
+void strqueue_insert_at(unsigned long id, size_t position, const char* str) {
+    // TODO
+}
+
+void strqueue_remove_at(unsigned long id, size_t position) {
+    // TODO
+}
+
+const char* strqueue_get_at(unsigned long id, size_t position) {
+    // TODO
+}
+
+void strqueue_clear(unsigned long id) {
+    // TODO    
+}
+
+int strqueue_comp(unsigned long id1, unsigned long id2) {
+    // TODO
+}
---
+++ strqueue.h
@@ -0,0 +1,33 @@
+#ifndef STRQUEUE_H
+#define STRQUEUE_H
+
+#include <stdio.h>
+#include <stdbool.h>
+
+#ifdef __cplusplus
+#include <iostream>
+namespace cxx {
+extern "C" {
+#endif
+
+    unsigned long strqueue_new();
+    void strqueue_delete(unsigned long id);
+    size_t strqueue_size(unsigned long id);
+    void strqueue_insert_at(unsigned long id, size_t position, const char* str);
+    void strqueue_remove_at(unsigned long id, size_t position);
+    const char* strqueue_get_at(unsigned long id, size_t position);
+    void strqueue_clear(unsigned long id);
+    int strqueue_comp(unsigned long id1, unsigned long id2);
+
+#ifdef __cplusplus
+}
+}
+#endif
+
+#endif // STRQUEUE_H
+
+#ifndef NDEBUG
+    const bool debug = true;
+#else
+    const bool debug = false;
+#endif
