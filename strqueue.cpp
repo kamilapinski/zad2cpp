2932206e8360cbc76d224838c55fec94e97dc491
Kamil Łapiński <kamill2004@gmail.com>
2024-10-21 18:35:59 +0200

    Dodano strqueue_insert_at

--- strqueue.cpp
+++ strqueue.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <unordered_map>
 #include <deque>
+#include <stack>
 
 using namespace std;
 
@@ -22,7 +23,35 @@ size_t strqueue_size(unsigned long id) {
 }
 
 void strqueue_insert_at(unsigned long id, size_t position, const char* str) {
-    // TODO
+    if (debug) 
+        cerr << "strqueue_insert_at(" << id << ", " << position << ", " << str << ")\n";
+
+    auto it = Map.find(id);
+
+    if (it != Map.end() && str != NULL) {
+        auto MainQueue = it->second;
+        size_t i = MainQueue.size() - 1;
+
+        stack<string> SupportStack;
+
+        while (i >= position) {
+            SupportStack.push(MainQueue.back());
+            MainQueue.pop_back();
+            i--;
+        }
+
+        MainQueue.push_back(str);
+
+        while (!SupportStack.empty()) {
+            MainQueue.push_back(SupportStack.top());
+            SupportStack.pop();
+        }
+
+        if (debug)
+            cerr << "strqueue_insert_at done\n";
+    }
+    else if (debug)
+        cerr << "strqueue_insert_at failed\n";
 }
 
 void strqueue_remove_at(unsigned long id, size_t position) {
