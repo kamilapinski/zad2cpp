1bceaa8ba4221d297eb1db503ea684ea2a4bcea6
Kamil Łapiński <kamill2004@gmail.com>
2024-10-25 11:16:55 +0200

    Poprawka, kompiluje się

--- strqueue.h
+++ strqueue.h
@@ -5,7 +5,6 @@
 #include <stdbool.h>
 
 #ifdef __cplusplus
-namespace cxx {
 extern "C" {
 #endif
 
@@ -20,6 +19,18 @@ extern "C" {
 
 #ifdef __cplusplus
 }
+#endif
+
+#ifdef __cplusplus
+namespace cxx {
+    inline unsigned long strqueue_new() { return ::strqueue_new(); }
+    inline void strqueue_delete(unsigned long id) { ::strqueue_delete(id); }
+    inline size_t strqueue_size(unsigned long id) { return ::strqueue_size(id); }
+    inline void strqueue_insert_at(unsigned long id, size_t position, const char* str) { ::strqueue_insert_at(id, position, str); }
+    inline void strqueue_remove_at(unsigned long id, size_t position) { ::strqueue_remove_at(id, position); }
+    inline const char* strqueue_get_at(unsigned long id, size_t position) { return ::strqueue_get_at(id, position); }
+    inline void strqueue_clear(unsigned long id) { ::strqueue_clear(id); }
+    inline int strqueue_comp(unsigned long id1, unsigned long id2) { return ::strqueue_comp(id1, id2); }
 }
 #endif
 
